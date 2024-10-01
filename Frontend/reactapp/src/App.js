cad2b3eaf608be26726734e361cb9f40cc3e91f6
Barracuda447 <144940016+Barracuda447@users.noreply.github.com>
2024-09-30 22:47:22 -0600

    Wishlist UseState Page added

--- Frontend/reactapp/src/App.js
+++ Frontend/reactapp/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { useState } from 'react';
 
 class Card {
   constructor(image, title, price, link) {
@@ -23,6 +24,8 @@ class Card {
 }
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('home');
+
   const regCardData = [
     new Card(
       "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Eo_circle_blue_number-1.svg/2048px-Eo_circle_blue_number-1.svg.png",
@@ -53,13 +56,35 @@ function App() {
     ),
   ];
 
+
+  const wishlistCardData = [
+    new Card(
+      "https://as2.ftcdn.net/v2/jpg/00/99/53/31/1000_F_99533164_fpE2O6vEjnXgYhonMyYBGtGUFCLqfTWA.jpg",
+      "Product Title 1",
+      "$29.99",
+      "http://localhost:3000/"
+    ),
+    new Card(
+      "https://static.vecteezy.com/system/resources/thumbnails/011/287/998/small_2x/number-2-3d-gold-png.png",
+      "Product Title 2",
+      "$29.99",
+      "http://localhost:3000/"
+    ),
+  ];
+
+
+
+  const handleNavigation = (page) => {
+    setCurrentPage(page);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <div className="navbar">
           <h1>Generic Store Name</h1>
-          <a href="http://localhost:3000/">Home</a>
-          <a href="http://localhost:3000/">Back to School</a>
+          <a href="#" onClick={() => handleNavigation('home')}>Home</a>
+          <a href="#" onClick={() => handleNavigation('wishlist')}>My Wishlist</a>
           <a href="http://localhost:3000/">Special Offers</a>
           <a href="http://localhost:3000/">Other Cool Menu Thing</a>
           <a href="http://localhost:3000/">Our Locations</a>
@@ -67,14 +92,36 @@ function App() {
         </div>
       </header>
       <body>
-        <h1>On Sale</h1>
-        <div id="regCards">
-          {regCardData.map(card => card.render())}
-        </div>
-        <h1>Products You Might Like</h1>
-        <div id="recommendedCards">
-          {recommendedCardData.map(card => card.render())}
-        </div>
+
+{/* HOME PAGE */}
+
+
+        {currentPage === 'home' && (
+          <>
+            <h1>On Sale</h1>
+            <div id="regCards">
+              {regCardData.map(card => card.render())}
+            </div>
+            <h1>Products You Might Like</h1>
+            <div id="recommendedCards">
+              {recommendedCardData.map(card => card.render())}
+            </div>
+          </>
+        )}
+
+
+
+{/* WISHLIST */}
+
+
+        {currentPage === 'wishlist' && (
+          <div>
+            <h1>My Wishlist</h1>
+            <div id="recommendedCards">
+              {wishlistCardData.map(card => card.render())}
+            </div>
+          </div>
+        )}
       </body>
       <footer>
         <h3>Contact us at 123-456-7890</h3>

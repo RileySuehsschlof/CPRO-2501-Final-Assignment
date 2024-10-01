fd5cdb62c20829cebdf1fa5c64217357c75ea4da
Barracuda447 <144940016+Barracuda447@users.noreply.github.com>
2024-09-30 21:07:44 -0600

    And so it begins
    Landing page has begun with use of 2 of the colors from our color theory. Header has been made and is sticky with buttons which have working links. They just lead back to the same page.

--- Frontend/reactapp/src/App.js
+++ Frontend/reactapp/src/App.js
@@ -1,13 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Learn React
-        </p>
+        <div class="navbar">
+            <h1>Generic Store Name</h1>
+            
+            <a href="http://localhost:3000/">Home</a>
+            <a href="http://localhost:3000/">Back to School</a>
+            <a href="http://localhost:3000/">Special Offers</a>
+            <a href="http://localhost:3000/">Other Cool Menu Thing</a>
+            <a href="http://localhost:3000/">Our Locations</a>
+            <a href="http://localhost:3000/">Cart</a>
+        </div>
       </header>
       <body>
 

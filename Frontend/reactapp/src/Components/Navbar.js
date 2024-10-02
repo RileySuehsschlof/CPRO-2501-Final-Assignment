a39e96ac74530f12b6e13cae4f8fd84504b98d1d
Barracuda447 <144940016+Barracuda447@users.noreply.github.com>
2024-10-02 17:39:52 -0600

    Navbar Component
    Same thing as footer and card, just with navbar.

---
+++ Frontend/reactapp/src/Components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Navbar = ({ onNavigate }) => {
+  return (
+    <header className="App-header">
+      <div className="navbar">
+        <h1>Generic Store Name</h1>
+        <a href="#" onClick={() => onNavigate('home')}>Home</a>
+        <a href="#" onClick={() => onNavigate('wishlist')}>My Wishlist</a>
+        <a href="http://localhost:3000/">Special Offers</a>
+        <a href="http://localhost:3000/">Our Locations</a>
+        <a href="http://localhost:3000/">Profile</a>
+        <a href="http://localhost:3000/">Cart</a>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;

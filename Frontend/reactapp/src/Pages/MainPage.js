4792db582607f540aef69a2808ceff64c2fd9f8c
Barracuda447 <144940016+Barracuda447@users.noreply.github.com>
2024-10-02 17:53:23 -0600

    Main Page assembled similar to the components.
    The only difference is that the card mapping had to be altered to be modified to a component-based architecture as the cards are no longer objects within the same file.

---
+++ Frontend/reactapp/src/Pages/MainPage.js
@@ -0,0 +1,26 @@
+// src/pages/MainPage.js
+
+import React from 'react';
+import Card from './components/Card';
+import './MainPage.css';
+
+const MainPage = ({ regCardData, recommendedCardData }) => {
+  return (
+    <div className="main-page">
+      <h1>On Sale</h1>
+      <div id="regCards">
+        {regCardData.map((data, index) => (
+          <Card key={index} {...data} />
+        ))}
+      </div>
+      <h1>Products You Might Like</h1>
+      <div id="recommendedCards">
+        {recommendedCardData.map((data, index) => (
+          <Card key={index} {...data} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MainPage;

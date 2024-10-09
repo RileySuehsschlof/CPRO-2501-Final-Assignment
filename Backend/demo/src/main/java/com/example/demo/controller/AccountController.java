b4af24b00ccc3eb625996c4736c5852916c4f1bf
Profundis-clamavi <144939360+Profundis-clamavi@users.noreply.github.com>
2024-10-09 12:04:38 -0600

    Added url option for accounts

--- Backend/demo/src/main/java/com/example/demo/controller/AccountController.java
+++ Backend/demo/src/main/java/com/example/demo/controller/AccountController.java
@@ -24,7 +24,7 @@ public class AccountController {
     }
 
     // Retrieve all accounts
-    @GetMapping("/accounts")
+    @GetMapping({"/accounts","/accounts/"})
     public List<Account> getAllAccounts() {
         return accountService.getAllAccounts();
     }

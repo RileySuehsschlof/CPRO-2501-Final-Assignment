4e5ba64fb8731d48742c1d3c1b5f386dffd68950
Ronny-Logan <justin.billetski@rdpolytech.ca>
2024-10-10 17:41:48 -0600

    Create service layer

---
+++ Backend/demo/src/main/java/com/example/demo/service/CartService.java
@@ -0,0 +1,34 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Cart;
+import com.example.demo.repository.ICartRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CartService {
+
+    @Autowired
+    ICartRepository repository;
+
+    public List<Cart> getAllCarts() {
+        return repository.findAll();
+    }
+
+    public Optional<Cart> getCartById(Integer id) {
+        return repository.findById(id);
+    }
+
+    public Cart saveCart(Cart cart) {
+        // You can add business logic here, e.g., calculating totalPrice
+        return repository.save(cart);
+    }
+
+    public void deleteCartById(Integer id) {
+        repository.deleteById(id);
+    }
+
+}

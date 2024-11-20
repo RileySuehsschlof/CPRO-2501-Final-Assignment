b27938d584e113047d4c601b58be84058b5952f8
RileySuehsschlof <144819266+RileySuehsschlof@users.noreply.github.com>
2024-11-20 11:09:38 -0700

    PaymentDetails page
    Added a page to get shippingAddress, billingAddress, cardNumber. Validates inputs, then pushes to database

--- Frontend/reactapp/src/CreateAccPage/SecondaryInfo.js
+++ Frontend/reactapp/src/CreateAccPage/SecondaryInfo.js
@@ -2,23 +2,104 @@ import CreateField from "../Components/Field";
 import CreateButton from "../Components/Button";
 import "./CreateAccCSS.css";
 import { useLocation } from "react-router-dom";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-function redirectToMainPage() {
-  console.log("Redirect to main page");
+function validate(value, name) {
+  //general function to check if any fields are empty then display message
+  if (value === "" || value === null) {
+    const error = `${name} is required.`;
+    return error;
+  }
+  return "";
+}
+function checkInputs() {
+  document.getElementById("shippingAddressError").innerHTML = "";
+  document.getElementById("billingAddressError").innerHTML = "";
+  document.getElementById("cardNumberError").innerHTML = "";
+
+  //gets values from form
+  const shippingInput = document
+    .getElementById("shippingAddressInput")
+    .value.trim();
+  const billingInput = document
+    .getElementById("billingAddressInput")
+    .value.trim();
+  const cardInput = document.getElementById("cardNumberInput").value.trim();
+
+  let validInputs = true;
+
+  //checks validation of form
+  const shippingError = validate(shippingInput, "Shipping Address");
+  if (shippingError) {
+    document.getElementById("shippingAddressError").innerHTML = shippingError;
+    validInputs = false;
+  }
+  const billingError = validate(billingInput, "Billing Address");
+  if (billingError) {
+    document.getElementById("billingAddressError").innerHTML = billingError;
+    validInputs = false;
+  }
+  const cardError = validate(cardInput, "Card Number");
+  if (cardError) {
+    document.getElementById("cardNumberError").innerHTML = cardError;
+    validInputs = false;
+  }
+  if (!isCardNumber(cardInput)) {
+    document.getElementById("cardNumberError").innerHTML =
+      "Invalid card number.";
+    return false;
+  }
+  return validInputs;
+}
+function isCardNumber(input) {
+  const cardRegex = /^[0-9]{16}$/;
+  return cardRegex.test(input);
+}
+function RedirectToMainPage(navigate) {
+  if (checkInputs()) {
+    let user = createUserData();
+    console.log(user);
+
+    axios
+      .post("http://localhost:8881/createaccount", user, {
+        headers: { "Content-Type": "application/json" },
+      })
+      .then((response) => {
+        console.log("User created", response.data);
+        navigate("/login");
+      })
+      .catch((error) => {
+        console.error("Error creating user", error);
+      });
+  }
+}
+function createUserData() {
+  let pastData = JSON.parse(localStorage.getItem("userData"));
+
+  let userData = {
+    email: pastData.email,
+    password: pastData.password,
+    name: pastData.name,
+    shippingAddress: document.getElementById("shippingAddressInput").value,
+    billingAddress: document.getElementById("billingAddressInput").value,
+    cardNumber: document.getElementById("cardNumberInput").value,
+  };
+  localStorage.removeItem("userData");
+  return userData;
 }
 
 function SecondaryInfo() {
   const location = useLocation();
   const userData = location.state;
-  console.log(userData);
+  const navigate = useNavigate();
+
+  localStorage.setItem("userData", JSON.stringify(userData));
   return (
     <div className="formContainer">
       <h1>Generic Website Name</h1>
       <form className="myForm">
-        <h2>Billing Details</h2>
-        {/* Templated element for a <p>, <input>, and <p> for error message */}
-        {/* Accepts title for the text to display, placeholderTxt for placeholder text, id for the elements id and errorId which is defined in the field.js */}
-        {/* also accepts type for the type of input you want */}
+        <h2>Payment Details</h2>
         <CreateField
           title="Shipping Address"
           placeholderTxt="Enter shipping address"
@@ -36,7 +117,7 @@ function SecondaryInfo() {
         ></CreateField>
         <CreateButton
           text="Create"
-          functionName={redirectToMainPage}
+          functionName={() => RedirectToMainPage(navigate)}
         ></CreateButton>
       </form>
     </div>

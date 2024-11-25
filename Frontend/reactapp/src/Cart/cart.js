ee306bf3de8b829b3acc52db7db30f85d718fd0a
Ronny-Logan <justin.billetski@rdpolytech.ca>
2024-11-25 16:00:17 -0700

    Removed useless comment

--- Frontend/reactapp/src/Cart/cart.js
+++ Frontend/reactapp/src/Cart/cart.js
@@ -5,13 +5,16 @@ import CheckoutButton from "../Stripe/CheckoutButton";
 
 const Cart = () => {
   const [cart, setCart] = useState([]);
-  //const userEmail = sessionStorage.getItem("userEmail");
+
+  //const token = sessionStorage.getItem("authToken");
+  //const [header, payload, signature] = token.split(".");
+  //const decodedPayload = JSON.parse(atob(payload));
   const userEmail = "fff@gmail.com";
 
   const fetchCart = useCallback(async () => {
     try {
       const encodedEmail = encodeURIComponent(userEmail);
-      const response = await axios.get(`http://localhost:8881/cart/${encodedEmail}`); // Was/cart/${encodedEmail}, but returned 404
+      const response = await axios.get(`http://localhost:8881/cart/${encodedEmail}`);
       setCart(response.data.items || []);
     } catch (error) {
       console.error("Error fetching cart:", error);
@@ -25,15 +28,16 @@ const Cart = () => {
 
   const addItemToCart = async (cartId, productId, quantity) => {
     try {
-        const response = await axios.post(`/cart/${cartId}/add-item`, {
-            productId,
-            quantity,
+        const response = await axios.post(`http://localhost:8881/cart/${cartId}/add-item`, {
+            cartId: cartId,
+            productId: productId,
+            quantity: quantity
         });
         console.log("Item added:", response.data);
     } catch (error) {
         console.error("Error adding item to cart:", error);
     }
-};
+  };
 
 
   return (

c8578452b5666dcfb5e622bd38c5eb7bcc8fbf65
RileySuehsschlof <144819266+RileySuehsschlof@users.noreply.github.com>
2024-11-28 19:33:56 -0700

    Account Tests
    Added tests for adding an account failing because email is already in use, and a test for logging in successfully

---
+++ Frontend/reactapp/tests/failCreateAcc.spec.js
@@ -0,0 +1,19 @@
+import { test, expect } from "@playwright/test";
+
+test("failCreateAcc", async ({ page }) => {
+  await page.goto("http://localhost:3000/");
+  await page.getByRole("link", { name: "Account", href: "#" }).click();
+  await page.getByRole("link", { name: "Create Account" }).click();
+  await page.getByPlaceholder("First and Last Name").click();
+  await page.getByPlaceholder("First and Last Name").fill("Riley Suehsschlof");
+  await page.getByPlaceholder("First and Last Name").press("Tab");
+  await page.getByPlaceholder("example@email.com").fill("smith@email.com");
+  await page.getByPlaceholder("example@email.com").press("Tab");
+  await page.getByPlaceholder("At least 6 characters").fill("smithy");
+  await page.getByPlaceholder("At least 6 characters").press("Tab");
+  await page.locator("#passwordAgainInput").fill("smithy");
+  await page.getByRole("button", { name: "Continue" }).click();
+  await expect(page.locator("#emailError")).toContainText(
+    "User with that email already exists."
+  );
+});
